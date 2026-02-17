316cc912bcc81c26b8864cef93edba2dc23cf927
Pragathisvaran2004 <sjkpragathisv@gmail.com>
2026-02-17 13:34:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from flask import Flask, request, jsonify
 import joblib
 app = Flask(__name__)
 model_filename = 'xgboost_weather_model.joblib'
+model = joblib.load('xgboost_weather_model.joblib')
+
 joblib.dump(model, model_filename)
 
 print(f"XGBoost model exported successfully to {model_filename}")
 
 # Load the trained XGBoost model
-model = joblib.load('xgboost_weather_model.joblib')
-
 # Preprocessing artifacts from previous steps
 column_means = {'u100': 1.386060554770142, 'v100': -0.6588336918310846, 'u10': 0.9796831334499894, 'v10': -0.3308165255835449, 'fg10': 6.83838937049919, 'ssrd': 820373.8757136638, 'msl': 100949.65353589428, 'sp': 51752.623034851786, 'd2m': 295.8783578191499, 't2m': 301.7398199353428, 'relative humidity': 67.34455302207745, 'cloud cover': 71.91277596815056, 'temperature': 28.668226967466925, 'wind direction 10m': 211.93139502151445, 'wind speed 10m': 10.074023806651383}
 feature_columns = ['u100', 'v100', 'u10', 'v10', 'fg10', 'ssrd', 'msl', 'sp', 'd2m', 't2m', 'relative humidity', 'cloud cover', 'temperature', 'wind direction 10m', 'wind speed 10m', 'year', 'month', 'day', 'hour']
